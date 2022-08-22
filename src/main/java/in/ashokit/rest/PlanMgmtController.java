3e837d4b01f10d0b7abbdc6e7846aa3d049a6160
Sushir1997 <103164652+Sushir1997@users.noreply.github.com>
2022-08-22 12:25:08 +0530

    Sop statement deleted

--- src/main/java/in/ashokit/rest/PlanMgmtController.java
+++ src/main/java/in/ashokit/rest/PlanMgmtController.java
@@ -18,12 +18,6 @@ public class PlanMgmtController {
 	private PlanMgmtServiceImpl service;
 @PostMapping("/createplan")
 	public String createPlan(@RequestBody  PlanDtlsEntityBinding binding) {
-		System.out.println("Post mapping started");
-		System.out.println("Post mapping started");
-		System.out.println("Post mapping started1");
-		System.out.println("Post mapping started2");
-		System.out.println("Post mapping started3");
-		System.out.println("Post mapping started3");
 	 PlanDtlsEntity entity=new  PlanDtlsEntity();
 	 entity.setPlanName(binding.getPlanName());
 	 entity.setPlanCategory(binding.getPlanCategory());
